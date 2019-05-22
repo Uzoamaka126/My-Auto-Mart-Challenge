0acc829f1ca104e37a937d7f26ee9e29aacff050
Uzoamaka126 <vanessapeters126@gmail.com>
2019-05-22 15:04:07 +0100

    [Feature #PT-166127280] API Folder created

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,3 +1,19 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "airbnb-base",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "linebreak-style": ["error", "windows"],
+        "indent": ["error", 2]
+    }
 };
