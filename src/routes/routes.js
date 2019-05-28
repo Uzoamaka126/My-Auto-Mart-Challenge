1e1c10fc31c5c2b3e2fd1562e62696c7d4767766
Uzoamaka126 <vanessapeters126@gmail.com>
2019-05-28 16:26:56 +0100

    [chore #166291235] setup travis

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import car from '../controllers/cars';
 import Users from '../controllers/user';
+import Orders from '../controllers/order';
 
 const router = express.Router();
 
@@ -10,9 +11,9 @@ router.post('/auth/signup', Users.createUsers);
 // router.get('/car', car.getAllCars);
 router.get('/car', car.carStatus);
 router.delete('/car/:id', car.deleteCar);
-// router.patch('/car/:id', car.updateStatus);
-// router.post('/order', order.createOrder);
-// router.get('/order/:id', order.getOrder);
-// router.patch('/order/:id', order.updateOrder);
+router.patch('/car/:id', car.updateStatus);
+router.post('/order', Orders.createOrder);
+router.get('/order/:id', Orders.getOrder);
+router.patch('/order/:id', Orders.updateOrder);
 
 export default router;
