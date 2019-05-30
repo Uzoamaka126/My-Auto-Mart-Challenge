ace86432bca193eb2afcb8b1cb9b245e5bf29ca6
Uzoamaka126 <vanessapeters126@gmail.com>
2019-05-30 13:58:06 +0100

    update of README file

--- src/test/order.test.js
+++ src/test/order.test.js
@@ -19,6 +19,14 @@ const orderDetails = {
   price_offered: 3500000,
 };
 
+const updatedOrder = {
+  id: 201,
+  car_id: 100,
+  status: 'Pending',
+  old_price_offered: 40000000,
+  new_price_offered: 380000000,
+};
+
 describe('Create an order', () => {
   it('/api/v1/order should return a 201 status code and create an order', (done) => {
     chai.request(app)
@@ -42,6 +50,36 @@ describe('Create an order', () => {
         if (err) return done(err);
         expect(res.status).to.equal(200);
         expect(res.body.message).to.eql('The order has been successfully retrieved');
+        this.setTimeout(500);
+        setTimeout(done, 300);
+      });
+  });
+
+  it('/api/v1/order/:id should respond with status code 201 and update the order', (done) => {
+    const id = 201;
+    chai.request(app)
+      .patch(`/api/v1/order/${id}`)
+      .set('Accept', 'application/json')
+      .send(updatedOrder)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.status).to.equal(201);
+        expect(res.body.message).to.eql('Order Updated successfully');
+        done();
+      });
+  });
+
+  it('/api/v1/order/:id/price should respond with status code 404 and and show order not found', (done) => {
+    const id = 201;
+    chai.request(app)
+      .patch(`/api/v1/order/${id}/price`)
+      .send({
+        new_price_offered: 50000000,
+      })
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.status).to.equal(404);
         done();
       });
   });
